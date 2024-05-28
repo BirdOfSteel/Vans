9fa1f2c887d9cea2a83f9f7388b32a2e4208eaf1
BirdOfSteel <AmirAziz2001@Gmail.com>
2024-05-28 07:30:50 +0100

    Removed old files and configure for firebase

--- api.js
+++ api.js
@@ -1,3 +1,22 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite"
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB36UGA1yvOtt9kI_2Z2u6iD7e5qiQToAY",
+  authDomain: "vanlife-fdf2a.firebaseapp.com",
+  projectId: "vanlife-fdf2a",
+  storageBucket: "vanlife-fdf2a.appspot.com",
+  messagingSenderId: "927488280382",
+  appId: "1:927488280382:web:693d65c983a9ffab998e6f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
